60ce27127b3f16ec2067435f7b8bc9b7d9b36ce7
CJ Quintero <cquintero4@islander.tamucc.edu>
2025-01-31 10:33:49 -0600

    testing

---
+++ src/TestingCodeOne.java
@@ -0,0 +1,9 @@
+public class TestingCodeOne {
+    public static void main(String[] args) 
+    {
+        System.out.println("\nHello, World!");
+        System.out.print("This is being written after I spent ");
+        System.out.println("30 minutes trying to get C# to work in VSC");
+        System.out.println("This is being coded in Java btw.");
+    }
+}
