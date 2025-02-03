21316e46996cf52b5a479782c1dbac6de6815bc6
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-02-03 16:49:08 -0600

    Refactor Main to delegate to MainMenu and add file creation.
    Updated `Main` to delegate execution to `MainMenu`. Enhanced `MainMenu` to support creating text files with user-provided names and saving input content, improving functionality for note and journal management. Added error handling for file operations.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Visual Studio Code!");
+        MainMenu.main(args);
     }
 }
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainMenu {
     public static void main(String[] args) {
@@ -25,6 +27,8 @@ public class MainMenu {
 
         int choice;
         String input;
+        String docName;
+        String fileName;
 
         System.out.println("New");
         System.out.println("-------------------------------------------------");
@@ -34,17 +38,40 @@ public class MainMenu {
 
         choice = scanner.nextInt();
 
+        //Print out message appropriate for the choice prompting user to enter a name for the file
         switch(choice){
             case 1:
-                System.out.println("New Note");
+                System.out.println("Give a name for a new note: ");
                 break;
             case 2:
-                System.out.println("New Journal");
+                System.out.println("Give a name for a new journal: ");
                 break;
         }
-        System.out.println("-------------------------------------------------");
+
+        //Takes input from user to create a .txt file based off the name given
+        docName = scanner.next();
+        fileName = docName + ".txt";
+
+        //User enters note body here
+        System.out.println("Enter your text here: ");
         input = scanner.next();
+        scanner.close();
+
+        //Prints out a bare-bone interface to for user to visualize a note
+        System.out.println(docName);
+        System.out.println("-------------------------------------------------");
         System.out.println(input);
         System.out.println("-------------------------------------------------");
+        /*
+        Saves the input into the working directory named after the filename
+        need to make a folder for notes and a folder for journals to hold the saved files for now
+        */
+
+        try (FileWriter newNote = new FileWriter(fileName)) {
+            newNote.write(input);
+            System.out.println("File was saved to: " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
