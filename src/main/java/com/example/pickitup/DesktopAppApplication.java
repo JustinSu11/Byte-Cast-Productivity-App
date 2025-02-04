a42c21725e064a37e6c72521f8fec36ca54f8319
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-02-04 14:13:05 -0600

    Set up folder structure for Maven

---
+++ src/main/java/com/example/pickitup/DesktopAppApplication.java
@@ -0,0 +1,25 @@
+package com.example.pickitup;
+
+import com.example.pickitup.ui.MainFrame;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import javax.swing.SwingUtilities;
+
+@SpringBootApplication // Enables Spring Boot autoconfiguration and component scanning
+public class DesktopAppApplication {
+
+    public static void main(String[] args){
+        //Start the Spring Boot application, which initializes the application context.
+        ConfigurableApplicationContext context = SpringApplication.run(DesktopAppApplication.class, args);
+
+        //Launch the swing UI on the event Dispatch Thread to ensure thread safety.
+        SwingUtilities.invokeLater(()->{
+            //Retrieve the MainFrame bean from the Spring context.
+            MainFrame frame = context.getBean(MainFrame.class);
+            //Make the main application window visible.
+            frame.setVisible(true);
+        });
+    }
+
+}
